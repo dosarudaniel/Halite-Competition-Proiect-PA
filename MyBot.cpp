905c7cc11087ac01a253848a298eb333b54b6dc2
Daniel-Florin Dosaru <dosarudaniel@yahoo.com>
2017-03-07 20:49:14 +0200

    Creating my first Bot in MyBot.cpp I think it is a good point to start building a new one. You can upload this file: 1488912301-4202324323.hlt here: https://halite.io/local_visualizer.php to visualize it's performance.

--- MyBot.cpp
+++ MyBot.cpp
@@ -9,12 +9,24 @@
 #include "hlt.hpp"
 #include "networking.hpp"
 
+#define POSSIBLE_MOVEMENTS 5
+
 int main() {
     srand(time(NULL));
 
     std::cout.sync_with_stdio(0);
 
     unsigned char myID;
+    float quality;
+    float max = 0;
+    int move = 0;
+
+    hlt::Location location;
+    hlt::Site neighbour;
+    hlt::Site home;
+
+
+    
     hlt::GameMap presentMap;
     getInit(myID, presentMap);
     sendInit("MyC++Bot");
@@ -22,13 +34,55 @@ int main() {
     std::set<hlt::Move> moves;
     while(true) {
         moves.clear();
-
         getFrame(presentMap);
 
         for(unsigned short a = 0; a < presentMap.height; a++) {
             for(unsigned short b = 0; b < presentMap.width; b++) {
-                if (presentMap.getSite({ b, a }).owner == myID) {
-                    moves.insert({ { b, a }, (unsigned char)(rand() % 5) });
+                home = presentMap.getSite({ b, a });
+                if (home.owner == myID) {
+                    location.x = b;
+                    location.y = a;
+                    quality = 0;
+                    max = 0;
+                    move = 0;
+
+                    // Calculate the best place to go:
+                    // Best place to go == max production/strength raport and strength less than the present place 
+
+                    for (int i = 1; i < POSSIBLE_MOVEMENTS; i++) {
+                        neighbour = presentMap.getSite(presentMap.getLocation(location, i));
+                        if(neighbour.owner != myID && neighbour.strength < home.strength){
+                            if(neighbour.strength > 0){
+                                quality = (float) neighbour.production / neighbour.strength;
+                            }
+                            else{
+                                quality = (float)9999999;
+                                move = i;
+                            }
+                            if (quality > max){
+                                move = i;
+                            }
+                        }
+                        
+                    }
+
+                     
+                    //TO DO: Find a better strategy for pieces in the center of my set of pieces,
+                    //      such as: a special "depth traversal"
+                    //      or move pieces from center to the edges.
+
+                    // If I just decided to stay home (STILL),
+                    if(rand()%5 == 1){
+                        for (int i = 1; i < POSSIBLE_MOVEMENTS; i++) {
+                            neighbour = presentMap.getSite(presentMap.getLocation(location, i));
+                            if(move == STILL && neighbour.owner == myID){
+                                //Combine the strength of two pieces
+                                move = i;
+                            }
+                        }
+                    }
+
+                    moves.insert({{b, a}, (unsigned char)(move) });
                 }
             }
         }
--- runGame.sh
+++ runGame.sh
@@ -2,4 +2,4 @@
 
 g++ -std=c++11 MyBot.cpp -o MyBot.o
 g++ -std=c++11 RandomBot.cpp -o RandomBot.o
-./halite -d "30 30" "./RandomBot.o" 
+./halite -d "50 50" -s 4202324323 "./MyBot.o" 
