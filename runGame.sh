25e2063ba30b1de2e7742a0b409a3dc4a5e53b8f
Daniel-Florin Dosaru <dosarudaniel@yahoo.com>
2017-04-09 01:12:02 +0300

    Script pentru etapa 2 adaugat. Bot-ul cu 191-192 de mutari adaugat.

--- runGame.sh
+++ runGame.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-g++ -std=c++11 MyBot.cpp -o MyBot.o
-g++ -std=c++11 RandomBot.cpp -o RandomBot.o
-./halite -d "50 50" -s 42 "./MyBot.o" 
+
+# Stage 2
+# Objective: Beat DBot_linux_64bit
+
+# Înălțime hartă	Lungime hartă	Seed	Punctaj
+#	28					24			314			0.1
+#	30					30			 42			0.1
+#	40					40			154			0.1
+#	30					50			  3			0.1
+# 	50					50			 42			0.1
+
+g++ -std=c++11 BestBot.cpp -o BestBot
+./halite -d "28 24" -s 314 "./BestBot" "./DBot_linux_64bit" 
+./halite -d "30 30" -s 42 "./BestBot" "./DBot_linux_64bit" 
+./halite -d "40 40" -s 154 "./DBot_linux_64bit" "./BestBot"
+./halite -d "30 50" -s 3 "./DBot_linux_64bit" "./BestBot"
+./halite -d "50 50" -s 42 "./DBot_linux_64bit" "./BestBot"
+# Uncomment if you to see how good is DBot_linux_64bit 
+#./halite -d "50 50" -s 42 "./DBot_linux_64bit"
